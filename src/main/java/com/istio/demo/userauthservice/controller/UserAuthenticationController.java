8a13430a8e7cf000e7b0b4d33e577adb5f17f54b
Muhammad Afaque Khan <mafaqkhan@muhammads-air.localdomain>
2023-07-18 17:01:51 +0200

    HttpClientErrorException handling

--- src/main/java/com/istio/demo/userauthservice/controller/UserAuthenticationController.java
+++ src/main/java/com/istio/demo/userauthservice/controller/UserAuthenticationController.java
@@ -4,13 +4,15 @@ import com.istio.demo.userauthservice.Model.UserCredentials;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -32,20 +34,24 @@ public class UserAuthenticationController {
     private String fetchUserDataFromSecondMicroservice(String username) {
         String secondMicroserviceBaseUrl = "http://" + userDataServiceUrl+ "/user_data/" + username;
         log.info("URI: "+secondMicroserviceBaseUrl);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                secondMicroserviceBaseUrl,
-                HttpMethod.GET,
-                null,
-                String.class
-        );
-        log.info("Response: " + responseEntity.toString());
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    secondMicroserviceBaseUrl,
+                    HttpMethod.GET,
+                    null,
+                    String.class
+            );
+            log.info("Response: " + responseEntity);
 
-        String responseBody = null;//responseEntity.getBody();
-        if (responseBody != null) {
-            return responseBody;
-        } else {
-            // Handle the case when the response body is null (empty response)
-            return "Empty response from user-data-service";
+            String responseBody = responseEntity.getBody();
+            // Check if the response body is null (empty response)
+            return Objects.requireNonNullElse(responseBody, "Empty response from user-data-service");
+
+        } catch (HttpClientErrorException ex) {
+            // Handle the HttpClientErrorException
+            // For example, you can log the error or return a specific error message
+            log.error("HttpClientErrorException: " + ex.getMessage());
+            return "Error: Unable to fetch user data from user-data-service";
         }
     }
 }
