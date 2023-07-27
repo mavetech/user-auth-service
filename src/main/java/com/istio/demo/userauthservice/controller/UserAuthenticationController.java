ac429f2413b2b28476cd0820b93f7658a9812308
Muhammad Afaque Khan <mafaqkhan@muhammads-air.localdomain>
2023-07-27 10:51:13 +0200

    refactor istio and controller

--- src/main/java/com/istio/demo/userauthservice/controller/UserAuthenticationController.java
+++ src/main/java/com/istio/demo/userauthservice/controller/UserAuthenticationController.java
@@ -28,17 +28,19 @@ public class UserAuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserCredentials credentials) {
         log.info("Inside /login controller");
-        String userDataResponse = fetchUserDataFromSecondMicroservice(credentials.getUsername());
-        if(userDataResponse.equals("Error: Unable to fetch user data from user-data-service")) {
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Error : Unable to fetch user data from user-data-service");
+        String userDataResponse = String.valueOf(fetchUserDataFromSecondMicroservice(credentials.getUsername()));
+        if (userDataResponse == null || userDataResponse.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: Empty response from user-data-service");
         }
+
         return ResponseEntity.ok(userDataResponse);
     }
 
-    private String fetchUserDataFromSecondMicroservice(String username) {
+    private ResponseEntity<String> fetchUserDataFromSecondMicroservice(String username) {
         String secondMicroserviceBaseUrl = "http://" + uri+ "/user_data/" + username;
-        log.info("URI: "+secondMicroserviceBaseUrl);
+        log.info("URI: " + secondMicroserviceBaseUrl);
         log.info("This is new!!!");
+
         try {
             ResponseEntity<String> responseEntity = restTemplate.exchange(
                     secondMicroserviceBaseUrl,
@@ -46,17 +48,26 @@ public class UserAuthenticationController {
                     null,
                     String.class
             );
+
             log.info("Response: " + responseEntity);
 
+            log.info("Status: " + responseEntity.getStatusCode());
+
+            // Check if the HTTP status code is 404 (Not Found)
+            if (responseEntity.getStatusCode() == HttpStatus.NOT_FOUND) {
+                // Handle the 404 case here (e.g., return a specific message)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empty response from user-data-service");
+            }
+
             String responseBody = responseEntity.getBody();
-            // Check if the response body is null (empty response)
-            return Objects.requireNonNullElse(responseBody, "Empty response from user-data-service");
+
+            return ResponseEntity.ok(responseBody);
 
         } catch (HttpClientErrorException ex) {
             // Handle the HttpClientErrorException
             // For example, you can log the error or return a specific error message
             log.error("HttpClientErrorException: " + ex.getMessage());
-            return "Error: Unable to fetch user data from user-data-service";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: Unable to fetch user data from user-data-service");
         }
     }
 }
